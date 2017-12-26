3b61b70a779c1080420aceea133b1101de444a21
svantebjorkberg <svante.bjorkberg@gmail.com>
2017-12-26 18:05:20 +0100

    First snake (not 100%)

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+    public int cellsX;
+    public int cellsY;
+    BoardCell[] boardCells;
+    private Snake snake;
+    public static float speed = 0.5f;
+    bool isInitialized = false;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    private void Initialize() 
+    {
+        snake = new Snake();
+
+        float boardWidth = GetComponent<MeshRenderer>().bounds.size.x;
+        float boardHeight = GetComponent<MeshRenderer>().bounds.size.y;
+
+        float snakeHeadWidth = snake.getSnakeHead().GetComponent<MeshRenderer>().bounds.size.x;
+        float snakeHeadHeight = snake.getSnakeHead().GetComponent<MeshRenderer>().bounds.size.y;
+        float cellSide = snakeHeadWidth > snakeHeadHeight ? snakeHeadWidth : snakeHeadHeight;
+
+        cellsX = (int)(boardWidth / cellSide);
+        cellsY = (int)(boardHeight / cellSide);
+
+        boardCells = new BoardCell[cellsX * cellsY];
+
+        for (int x = 0; x < cellsX; x++)
+        {
+            for (int y = 0; y < cellsY; y++)
+            {
+                int ix = x * cellsX + y;
+                boardCells[ix] = new BoardCell();
+            }
+        }
+
+        isInitialized = true;
+    }
+
+
+}
---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour {
+    private SnakeHead snakeHead;
+    public Snake()
+    {
+        snakeHead = new SnakeHead();
+    }
+
+    public SnakeHead getSnakeHead()
+    {
+        return snakeHead;
+    }
+}
